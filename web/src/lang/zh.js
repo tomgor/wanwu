78911b1937ca6fe600a92c2f35fb34f9abad7e5a
dysiliky <dysiliky@163.com>
2025-07-02 14:20:21 +0800

    chore(web): 智能体/工作流/文本问答卡片列表添加取消发布

--- web/src/lang/zh.js
+++ web/src/lang/zh.js
@@ -93,6 +93,7 @@ export default {
             import: '导入',
             export: '导出',
             view: '查看',
+            cancelPublish: '取消发布'
         },
         switch: {
             start: '开启',
