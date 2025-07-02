a1db62cbfedb97bd7974265b58ef6d6f7cd3cd6f
tayunzk <971988947@qq.com>
2025-07-02 11:49:24 +0800

    fix(bff):添加模型调用model参数校验

--- internal/bff-service/server/http/handler/router/callback/router.go
+++ internal/bff-service/server/http/handler/router/callback/router.go
@@ -13,5 +13,5 @@ func Register(openAPI *gin.RouterGroup) {
 	mid.Sub("callback").Reg(openAPI, "/model/:modelId", http.MethodGet, callback.GetModelById, "根据modelId获取模型")
 	mid.Sub("callback").Reg(openAPI, "/model/:modelId/chat/completions", http.MethodPost, callback.ModelChatCompletions, "Model Chat Completions")
 	mid.Sub("callback").Reg(openAPI, "/model/:modelId/embeddings", http.MethodPost, callback.ModelEmbeddings, "Model Embeddings")
-	mid.Sub("callback").Reg(openAPI, "/model/:modelId/rerank", http.MethodPost, callback.ModelRerank, "Model Embeddings")
+	mid.Sub("callback").Reg(openAPI, "/model/:modelId/rerank", http.MethodPost, callback.ModelRerank, "Model rerank")
 }
