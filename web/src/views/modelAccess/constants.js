20c34773b1c403312535e5ca7904509142383d6c
dysiliky <dysiliky@163.com>
2025-07-22 17:18:49 +0800

    chore(web): 增加模型供应商

--- web/src/views/modelAccess/constants.js
+++ web/src/views/modelAccess/constants.js
@@ -10,9 +10,23 @@ export const MODEL_TYPE = Object.keys(MODEL_TYPE_OBJ).map(key => ({key, name: MO
 
 export const YUAN_JING = 'YuanJing'
 export const OPENAI_API = 'OpenAI-API-compatible'
+export const OLLAMA = 'Ollama'
+export const QWEN = 'Qwen'
+export const HUOSHAN = 'Huoshan'
 export const PROVIDER_OBJ = {
     [OPENAI_API]: 'OpenAI-API-compatible',
-    [YUAN_JING]: '联通元景'
+    [YUAN_JING]: '联通元景',
+    [OLLAMA]: 'Ollama',
+    [QWEN]: '通义千问',
+    [HUOSHAN]: '火山引擎',
+}
+
+export const PROVIDER_IMG_OBJ = {
+    [OPENAI_API]: require('@/assets/imgs/openAI.png'),
+    [YUAN_JING]: require('@/assets/imgs/yuanjing.png'),
+    [OLLAMA]: require('@/assets/imgs/ollama.png'),
+    [QWEN]: require('@/assets/imgs/qwen.png'),
+    [HUOSHAN]: require('@/assets/imgs/volcano.png'),
 }
 
 export const PROVIDER_TYPE = Object.keys(PROVIDER_OBJ)
@@ -28,10 +42,16 @@ export const FUNC_CALLING = [
 export const TYPE_OBJ = {
     apiKey: {
         [YUAN_JING]: 'sk-abc********************xyz',
-        [OPENAI_API]: 'sk_7e4*************4s-BpI1l'
+        [OPENAI_API]: 'sk_7e4*************4s-BpI1l',
+        [OLLAMA]: '',
+        [QWEN]: 'sk-b************c70d',
+        [HUOSHAN]: 'd8008ac0-****-****-****-**************'
     },
     inferUrl: {
         [YUAN_JING]: 'https://maas.ai-yuanjing.com/openapi/compatible-mode/v1',
-        [OPENAI_API]: 'https://api.siliconflow.cn/v1'
+        [OPENAI_API]: 'https://api.siliconflow.cn/v1',
+        [OLLAMA]: 'https://192.168.21.100:11434',
+        [QWEN]: 'https://dashscope.aliyuncs.com/compatible-mode/v1',
+        [HUOSHAN]: 'https://ark.cn-beijing.volces.com/api/v3'
     },
 }
