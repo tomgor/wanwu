fd56117bf694973c941d3b6a04850410e0d2cab4
limei <17701216705@163.com>
2025-07-23 17:05:16 +0800

    fix(web):关键词联调

--- web/src/api/keyword.js
+++ web/src/api/keyword.js
@@ -2,7 +2,7 @@ import service from "@/utils/request";
 //查询知识库关键词列表
 export const getKeyWord = (data)=>{
     return service({
-        url: '/konwledgeServe/v1/knowledge/keywords',
+        url: '/user/api/v1/knowledge/keywords',
         method: 'get',
         params: data
     })
@@ -11,7 +11,7 @@ export const getKeyWord = (data)=>{
 //新增知识库关键词列表
 export const addKeyWord = (data)=>{
     return service({
-        url: '/konwledgeServe/v1/knowledge/keywords',
+        url: '/user/api/v1/knowledge/keywords',
         method: 'post',
         data
     })
@@ -19,7 +19,7 @@ export const addKeyWord = (data)=>{
 //编辑知识库关键词列表
 export const editKeyWord = (data)=>{
     return service({
-        url: '/konwledgeServe/v1/knowledge/keywords',
+        url: '/user/api/v1/knowledge/keywords',
         method: 'put',
         data
     })
@@ -27,7 +27,7 @@ export const editKeyWord = (data)=>{
 //删除知识库关键词列表
 export const delKeyWord = (data)=>{
     return service({
-        url: '/konwledgeServe/v1/knowledge/keywords',
+        url: '/user/api/v1/knowledge/keywords',
         method: 'delete',
         data
     })
@@ -35,7 +35,7 @@ export const delKeyWord = (data)=>{
 //知识库关键词详情
 export const keyWordDetail = (data)=>{
     return service({
-        url: '/konwledgeServe/v1/knowledge/keywords/detail',
+        url: '/user/api/v1/knowledge/keywords/detail',
         method: 'get',
         params: data
     })
