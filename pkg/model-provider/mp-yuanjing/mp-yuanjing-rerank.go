d456088016cbe73133082c609dd8d0ae196fcf33
gromitlee <lihao_offical@foxmail.com>
2025-07-29 21:12:15 +0800

    refactor(model): mp_common.Rerank

--- pkg/model-provider/mp-yuanjing/mp-yuanjing-rerank.go
+++ pkg/model-provider/mp-yuanjing/mp-yuanjing-rerank.go
@@ -2,15 +2,11 @@ package mp_yuanjing
 
 import (
 	"context"
-	"crypto/tls"
 	"encoding/json"
-	"fmt"
-	"io"
 	"net/url"
 
 	"github.com/UnicomAI/wanwu/pkg/log"
 	mp_common "github.com/UnicomAI/wanwu/pkg/model-provider/mp-common"
-	"github.com/go-resty/resty/v2"
 )
 
 type Rerank struct {
@@ -27,36 +23,9 @@ func (cfg *Rerank) NewReq(req *mp_common.RerankReq) (mp_common.IRerankReq, error
 }
 
 func (cfg *Rerank) Rerank(ctx context.Context, req mp_common.IRerankReq, headers ...mp_common.Header) (mp_common.IRerankResp, error) {
-	if cfg.ApiKey != "" {
-		headers = append(headers, mp_common.Header{
-			Key:   "Authorization",
-			Value: "Bearer " + cfg.ApiKey,
-		})
-	}
-
-	request := resty.New().
-		SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true}). // 关闭证书校验
-		SetTimeout(0).                                             // 关闭请求超时
-		R().
-		SetContext(ctx).
-		SetHeader("Content-Type", "application/json").
-		SetHeader("Accept", "application/json").
-		SetBody(req.Data()).
-		SetDoNotParseResponse(true)
-	for _, header := range headers {
-		request.SetHeader(header.Key, header.Value)
-	}
-
-	url := cfg.rerankUrl()
-	resp, err := request.Post(url)
+	b, err := mp_common.Rerank(ctx, "yuanjing", cfg.ApiKey, cfg.rerankUrl(), req.Data(), headers...)
 	if err != nil {
-		return nil, fmt.Errorf("request %v yuanjing rerank err: %v", url, err)
-	} else if resp.StatusCode() >= 300 {
-		return nil, fmt.Errorf("request %v yuanjing rerank http status %v msg: %v", url, resp.StatusCode(), resp.String())
-	}
-	b, err := io.ReadAll(resp.RawResponse.Body)
-	if err != nil {
-		return nil, fmt.Errorf("request %v yuanjing rerank read response body err: %v", url, err)
+		return nil, err
 	}
 	return &rerankResp{raw: string(b)}, nil
 }
