b38e345a65966575d1226b90db5e52cfa05c8c5b
dysiliky <dysiliky@163.com>
2025-08-05 16:28:19 +0800

    chore(web): 配置平台配置权限

--- web/src/router/constants.js
+++ web/src/router/constants.js
@@ -3,7 +3,7 @@ export const PERMS = {
     PERMISSION_USER: 'permission.user', // 权限管理-用户管理
     PERMISSION_ORG: 'permission.org', // 权限管理-组织管理
     PERMISSION_ROLE: 'permission.role', // 权限管理-角色管理
-    SETTING: '', // 平台配置 setting
+    SETTING: 'setting', // 平台配置
 
     EXPLORE: 'exploration', // 探索广场
     MODEL: 'model', // 模型管理
