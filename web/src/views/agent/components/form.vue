ad46b802d638853ddb68088e87e321d5f49e4d6d
limei <17701216705@163.com>
2025-09-04 14:52:00 +0800

    fix(web): 删除智能体工具中valid相关逻辑判断

--- web/src/views/agent/components/form.vue
+++ web/src/views/agent/components/form.vue
@@ -264,7 +264,6 @@
                       v-model="n.enable"
                       class="bt-switch"
                       @change="toolSwitch(n,n.type,n.enable)"
-                      v-if="n.valid"
                     ></el-switch>
                     <span
                       @click="toolRemove(n,n.type)"
@@ -576,7 +575,6 @@ export default {
       this.editForm.knowledgeConfig = data;
     },
     displayName(item) {
-      if (!item.valid) return `工具已失效`;
       const config = this.nameMap[item.type] || this.nameMap["default"];
       return item[config.propName] + ' ' + `(${config.displayName})`;
     },
